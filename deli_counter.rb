4b416510265689009e9b215cae79f5bec4d4288c
Joseph Reilly <reilly4181@gmail.com>
2019-04-09 18:42:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,9 @@ katz_deli = []
 def line(katz_deli)
   if  katz_deli.length == 0 
   puts  "The line is currently empty."
+  elsif  katz_line != 0
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_line.length} in line."
 
 end
 end
