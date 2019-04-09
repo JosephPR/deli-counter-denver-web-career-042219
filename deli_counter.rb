cd90f829c9e054d613b1c66b46ee67e19c5088c6
Joseph Reilly <reilly4181@gmail.com>
2019-04-09 18:53:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,31 @@
 katz_deli = []
-
+#line
 def line(katz_deli)
-  if  katz_deli.length == 0 
-  puts  "The line is currently empty."
-    elsif  katz_deli != 0
-  katz_deli.push(name)
-  puts "The line is currently:#{katz_deli.length}"
-
-
+  phrase = "The line is currently: "
+  if katz_deli.length > 0 
+    katz_deli.each_with_index do |name, index|
+      phrase += "#{index + 1}. #{name} "
+    end
+    puts phrase
+  else
+    puts "The line is currently empty."
+  end
 end
+
+#take a number
+def take_a_number(katz_deli, name)
+	katz_deli.push(name)
+	puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+#	end
 end
 
 
-def take_a_number (katz_deli, name)
-name.push(name)
-puts #{name}, #{katz_deli.length}
+#now serving
+def now_serving(katz_deli)
+  if katz_deli == 0
+    puts "There is nobody waiting to be served!"
+  else 
+	 puts "Currently serving " + katz_deli[0] +"."
+  end
+  katz_deli.shift
 end
