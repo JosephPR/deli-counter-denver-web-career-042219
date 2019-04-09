ae27625dbfe7727d4384ce645ac5c78679e333e2
Joseph Reilly <reilly4181@gmail.com>
2019-04-09 18:46:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,17 +3,20 @@ katz_deli = []
 def line(katz_deli)
   if  katz_deli.length == 0 
   puts  "The line is currently empty."
-  
-
-end
-end
-
-def take_a_number(katz_line, name) 
-  if  katz_deli.length == 0 
-  puts  "The line is currently empty."
-  elsif  katz_deli != 0
+    elsif  katz_deli != 0
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 
+
 end
 end
+
+# def take_a_number(katz_line, name) 
+#   if  katz_deli.length == 0 
+#   puts  "The line is currently empty."
+#   elsif  katz_deli != 0
+#   katz_deli.push(name)
+#   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+
+# end
+# end
